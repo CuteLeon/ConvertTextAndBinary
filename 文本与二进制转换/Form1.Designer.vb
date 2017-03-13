1267eddeea44f654d14b3a4466dbb764d84d83e4
Leon <Leon@LEONVO>
2017-03-13 21:42:02 +0800

    第一次提交

---
+++ 文本与二进制转换/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 160)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "转换为二进制"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(353, 143)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Test String"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 189)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(353, 143)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(276, 160)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "还原为文本"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Ascii", "UTF-8"})
+        Me.ComboBox1.Location = New System.Drawing.Point(151, 163)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(108, 165)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "编码："
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(377, 344)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "文本与二进制转换"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ 文本与二进制转换/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Dim EncodeTypes() As Encoding = {Encoding.ASCII, Encoding.UTF8, Encoding.Unicode}
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = vbNullString OrElse ComboBox1.SelectedIndex = -1 Then Exit Sub
+        TextBox2.Text = ConvertTextToBianry(TextBox1.Text, EncodeTypes(ComboBox1.SelectedIndex))
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox2.Text = vbNullString OrElse ComboBox1.SelectedIndex = -1 Then Exit Sub
+        TextBox1.Text = ConvertBianryToText(TextBox2.Text, EncodeTypes(ComboBox1.SelectedIndex))
+    End Sub
+
+    Private Function ConvertTextToBianry(IniString As String, EncodeType As Encoding) As String
+        Dim ResultString As String = vbNullString
+        Dim BitList As BitArray = New BitArray(EncodeType.GetBytes(IniString))
+        For Index As Integer = 0 To BitList.Length - 1 Step 8
+            For IndexInside As SByte = 7 To 0 Step -1
+                ResultString &= IIf(BitList.Item(Index + IndexInside), "1", "0")
+            Next
+            ResultString &= " "
+        Next
+        Return ResultString
+    End Function
+
+    Private Function ConvertBianryToText(IniString As String, EncodeType As Encoding) As String
+        Dim ResultString As String = vbNullString
+        Dim IString As String = IniString.Replace(" ", String.Empty)
+        Dim BitList As BitArray = New BitArray(IString.Length, False)
+        For Index As Integer = 0 To IString.Length - 8 Step 8
+            For IndexInside As Byte = 0 To 7
+                If IString.Chars(Index + 7 - IndexInside) = "1" Then BitList.Set(Index + IndexInside, True)
+            Next
+        Next
+        Dim Bytes(BitList.Length / 8 - 1) As Byte
+        BitList.CopyTo(Bytes, 0)
+        ResultString = EncodeType.GetString(Bytes)
+        Return ResultString
+    End Function
+End Class
